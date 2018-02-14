23eb8ece93997311df5360f72ab91a241ce392b2
EddyKlappe <eddyklappe@Eddys-MacBook-Pro.local>
2018-02-14 15:44:38 +0100

    Update 2

---
+++ UnityLes2/Assets/ColorChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+	
+	public MeshRenderer renderer;
+	public Color newColor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.E)) {
+			renderer.material.color = newColor;
+		}
+		
+	}
+}
